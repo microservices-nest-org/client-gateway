6307c99cbc51ed2e6c5a485e632a1f618b3eca5c
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2024-06-10 17:10:56 -0500

    Add initial project configuration files and modules

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ExceptionFilter } from './common';
+
+async function bootstrap() {
+  const logger = new Logger('gateway');
+
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new ExceptionFilter());
+
+  await app.listen(envs.port);
+
+  logger.log(`Server is running on: ${envs.port}`);
+}
+bootstrap();
